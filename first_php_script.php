2855572845619d7adf7c8d4e3d679f336a65ced2
jwilkinson6179 <JamesW6179@gmail.com>
2020-03-22 19:35:19 -0400

    Created a simple object and method

--- first_php_script.php
+++ first_php_script.php
@@ -9,14 +9,26 @@
     }
     fwrite(STDOUT, "You're out.\n");
 
-    fwrite(STDOUT, "What is your name (enter below):\n");
-    $userName = fread(STDIN, 80); // Read up to 80 characters or a newline
-    fwrite(STDOUT, 'Hello '.$userName."!\n\n");
-
     fwrite(STDOUT, "Test file to read:\n"."\n".$_SERVER["argv"][1]."\n");
 
     $fpath = fopen($_SERVER["argv"][1], "r");
     $fileStr = fread($fpath, filesize($_SERVER["argv"][1]));
     fclose($fpath);
     fwrite(STDOUT, $fileStr."\n\n");
+
+    class Person {
+        private $name;
+
+        public function __construct($name) {
+            $this->name = $name;
+        }
+
+        public function print_my_name() { echo "Hello, my name is ".$this->name."!\n"; }
+    }
+
+    fwrite(STDOUT, "What is your name (enter below):\n");
+    $userName = fread(STDIN, 80);
+
+    $personObj = new Person($userName);
+    $personObj->print_my_name();
 ?>
